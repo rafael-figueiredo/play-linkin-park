f73c9ff97c02d1c63c019c8277be44de61491796
rafael-figueiredo <rafael.figueiredo.2000@hotmail.com>
2017-06-01 16:35:20 -0300

    enviando pela primeira vez

---
+++ js/myplaylist.js
@@ -0,0 +1,62 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01.mp3',
+        title:'Lost in the echo',
+        artist:'Linkin Park',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:25',
+        cover:'mix/01.jpg'
+    },
+
+    {
+        mp3:'mix/02.mp3',
+        title:'Castle of glass',
+        artist:'Linkin Park',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:25',
+        cover:'mix/01.jpg'
+    },
+    {
+        mp3:'mix/03.mp3',
+        title:'New Divide',
+        artist:'Linkin Park',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:28',
+        cover:'mix/01.jpg'
+    },
+        {
+        mp3:'mix/04.mp3',
+        title:'The catalyst',
+        artist:'Linkin Park',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:43',
+        cover:'mix/01.jpg'
+    },
+        {
+        mp3:'mix/05.mp3',
+        title:'Numb',
+        artist:'Linkin Park',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:06',
+        cover:'mix/01.jpg'
+    },
+];
